a955353495fb45862b22e4c1e16de6ef437569d7
WangYang <2421630246@qq.com>
2019-04-03 16:54:37 +0800

    add new component-head

---
+++ myapp/src/components/head/index.jsx
@@ -0,0 +1,13 @@
+import React,{Component} from "react";
+
+class Head extends Component{
+    render() {
+        return (
+            <div>
+                <h2>Head Component</h2>
+            </div>
+        );
+    }
+}
+
+export default Head
